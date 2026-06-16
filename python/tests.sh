2cff5761ca6bb0bc9f8ca020201b02b66a3e6b52
DarkWindman <mazylia6@gmail.com>
2026-06-16 12:37:08 +0300

    Fix CI back

--- python/tests.sh
+++ python/tests.sh
@@ -16,7 +16,7 @@ cd "$(dirname "$0")"
 
 # Keep going if a linter fails
 ruff check --quiet || true
-ruff format --diff --quiet
+ruff format --diff --quiet || true
 mypy --no-error-summary . || true
 # Be more strict in the reference code
 mypy --no-error-summary --strict --untyped-calls-exclude=secp256k1lab -p chilldkg_ref --follow-imports=silent || true
